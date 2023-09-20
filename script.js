df7ad7a2aa285744795ab6555e54b891b16218f4
Faisel Khan <125124484+faizkhan1318@users.noreply.github.com>
2023-09-20 16:04:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const ul = document.getElementsByTagName("ul")[0] ;
+let displayText  ;
+
+for(let i = 0 ; i < ul.children.length; i++){ 
+  let node = ul.children[i] ;
+  if(node.id == "level"){
+		displayText = "The level of the element is: "+(i)
+	}
+}
+alert(displayText);
